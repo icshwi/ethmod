1630b6647c791f587f2d6940e030e95360cbb4b1
Hinko Kocevar <hinko.kocevar@esss.se>
2017-04-18 11:16:56 +0200

    No sleeps..

--- tests/read_voltage.sh
+++ tests/read_voltage.sh
@@ -7,8 +7,8 @@ cd $_pwd
 while [ 1 ]
 do
     caput ETHMOD:I2C1:VMon1:Trigger 1
-    sleep 1
+#    sleep 1
     caput ETHMOD:I2C1:VMon1:Read 1
-    sleep 1
+#    sleep 1
     caget ETHMOD:I2C1:VMon1:ValueV7_RBV ETHMOD:I2C1:VMon1:ValueV8_RBV
 done
